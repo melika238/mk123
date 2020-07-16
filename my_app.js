875d3980cc75e154ca71047fddcc8f5edc2340b2
melika238 <melikakouki0@gmail.com>
2020-07-16 10:28:58 +0100

    first commit

---
+++ my_app.js
@@ -0,0 +1,8 @@
+import react ,{useState, useEffect} from 'react';
+
+ const list=()=>{
+    const [list, setlist ]=
+     useState([{id:1,nomDuMovie:'aladdin',langue:'english',duree:'2h 8m'},
+     {id:2,nomDuMovie:'maleficent',langue:'english',duree:'1h 37m'},
+     {id:3,nomDuMovie:'beauty and the beast',langue:'english',duree:'1h 49m'}
+ ])}
